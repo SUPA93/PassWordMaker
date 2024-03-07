72b72e55e2deb2199f413064008419d3d9fb0550
SUPA93 <supaflavaz93@hotmail.com>
2024-03-08 00:36:06 +0100

    form handling and functions

--- index.js
+++ index.js
@@ -6,11 +6,26 @@ const dataNumbers = "0123456789";
 
 const dataSymbols = "$^*ù!;:,?&é\"'(-è_çà@=)~#{[|`\^]}/€`";
 
-
+const rangeValue = document.getElementById("password-length")
 
 function generatePassword() {
-    console.log('Je génére un mot de passe');
+    let data = [];
+    let password = "";
+    if (lowercase.checked) data.push(...dataLowercase);
+    if (uppercase.checked) data.push(...dataUppercase);
+    if (numbers.checked) data.push(...dataNumbers);
+    if (symbols.checked) data.push(...dataSymbols);
+
+    if (data.length === 0) {
+        alert("Please add a choice");
+        return;
+    }
+
+    for (i = 0; i < rangeValue.value; i++) {
+        password += data[Math.floor(Math.random() * data.length)];
+        console.log(password);
+    }
 
 }
 
-generateButton.addEventListener("click", generatePassword());
+generateButton.addEventListener("click", generatePassword);
