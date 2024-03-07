d6ed0fe715801c5d9b224050d15273694bde6f6d
SUPA93 <supaflavaz93@hotmail.com>
2024-03-07 12:52:23 +0100

    javascript on the build

---
+++ index.js
@@ -0,0 +1,16 @@
+const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
+
+const dataUppercase = dataLowercase.toUpperCase();
+
+const dataNumbers = "0123456789";
+
+const dataSymbols = "$^*ù!;:,?&é\"'(-è_çà@=)~#{[|`\^]}/€`";
+
+
+
+function generatePassword() {
+    console.log('Je génére un mot de passe');
+
+}
+
+generateButton.addEventListener("click", generatePassword());
